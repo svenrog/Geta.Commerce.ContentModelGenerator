d8ba2deec3b93e318b2c640186f940b9f17fe886
Sven-Erik Jonsson <svenerikj@msn.com>
2017-11-06 13:37:58 +0100

    Added parameter handling to example.

---
+++ src/Geta.Commerce.ContentModelGenerator.Demo/Options.cs
@@ -0,0 +1,34 @@
+﻿using CommandLine;
+using CommandLine.Text;
+
+namespace Geta.Commerce.ContentModelGenerator.Example
+{
+    public class Options
+    {
+        [Option('c', "connectionString", Required = false, HelpText = "Connection string to utilize.")]
+        public string ConnectionString { get; set; }
+
+        [Option('r', "connectionProvider", Required = false, HelpText = "Connection provider to utilize.")]
+        public string ConnectionProvider { get; set; }
+
+        [Option('b', "generateBaseClasses", Required = false, HelpText = "True if base classes are to be aggregated.")]
+        public bool GenerateBaseClasses { get; set; }
+
+        [Option('p', "path", Required = true, HelpText = "Output path.")]
+        public string Path { get; set; }
+
+        [Option('n', "namespace", Required = true, HelpText = "Base namespace.")]
+        public string NameSpace { get; set; }
+
+        [ParserState]
+        public IParserState LastParserState { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this,
+              (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+
+}
--- src/Geta.Commerce.ContentModelGenerator.Demo/Program.cs
+++ src/Geta.Commerce.ContentModelGenerator.Demo/Program.cs
@@ -1,36 +1,51 @@
 ﻿using System;
 using System.Configuration;
 using Geta.Commerce.ContentModelGenerator.Access;
+using CommandLine;
 
 namespace Geta.Commerce.ContentModelGenerator.Example
 {
     class Program
     {
+        const string _connectionName = "EcfSqlConnection";
+        const string _connectionDefaultProvider = "System.Data.SqlClient";
+
         static void Main(string[] args)
         {
-            if (args.Length < 2)
+            var options = new Options();
+            if (!Parser.Default.ParseArguments(args, options))
             {
-                Console.WriteLine("Supply arguments: Executeable {path} {namespace}");
+                Console.WriteLine("Supply arguments: -p {path} -n {namespace}, type -? for help");
                 return;
             }
 
             try
             {
-                var configuration = ConfigurationManager.ConnectionStrings["EcfSqlConnection"];
+                ConnectionStringSettings configuration;
+
+                if (string.IsNullOrEmpty(options.ConnectionString))
+                {
+                    configuration = ConfigurationManager.ConnectionStrings[_connectionName];
+                }
+                else
+                {
+                    configuration = new ConnectionStringSettings(_connectionName, options.ConnectionString, options.ConnectionProvider ?? _connectionDefaultProvider);
+                }
+
                 if (configuration == null) throw new ConfigurationErrorsException();
 
                 DataAccessBase.Initialize(configuration);
 
-                var exporter = new CommerceInRiverExporter(args[0], args[1])
+                var exporter = new CommerceInRiverExporter(options.Path, options.NameSpace)
                 {
-                    GenerateBaseClasses = true
+                    GenerateBaseClasses = options.GenerateBaseClasses
                 };
 
                 exporter.Export();
             }
             catch (ConfigurationErrorsException)
             {
-                Console.Write("Provide a connection with name 'EcfSqlConnection' in the application configuration.");
+                Console.Write("Provide a connection with name '{0}' in the application configuration or supply a -c argument.", _connectionName);
             }
             catch (Exception ex)
             {
